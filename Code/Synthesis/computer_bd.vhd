f2b609d149b1875da6e6e6e6fa7757978ab6f880
JetStarBlues <JetStarBlues@users.noreply.github.com>
2017-08-13 20:52:02 -0600

    reorg

---
+++ Code/Synthesis/computer_bd.vhd
@@ -0,0 +1,51 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use work.components_pk.all;
+
+
+entity computer_bd is
+
+	port (
+
+		clk   : in  std_logic;
+		reset : in  std_logic;
+		tx    : out std_logic
+	);
+
+end entity;
+
+
+architecture ac of computer_bd is
+
+	signal outputReady  : std_logic;
+	signal outputRegOut : std_logic_vector( N - 1 downto 0 );
+
+begin
+
+	comp_computer : computer port map (
+
+		clk,
+		reset,
+		outputReady,
+		outputRegOut
+	);
+
+	comp_uartTX : UART_TX
+	generic map (
+
+		434  -- 50M Hz / 115200 Hz
+	)
+	port map (
+
+		clk,
+		outputReady,
+		outputRegOut,
+		tx,
+		open,
+		open
+	);
+
+end architecture;
+
+
+--
